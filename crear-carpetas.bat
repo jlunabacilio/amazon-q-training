983f4419fac3bbcec157e4abb9deda3dda1997c0
jluna <josea.luna@softtek.com>
2026-01-06 08:12:28 -0800

    second version

---
+++ crear-carpetas.bat
@@ -0,0 +1,5 @@
+@echo off
+mkdir src\main\java\com\example\flightreservations
+mkdir src\main\resources
+echo Carpetas creadas exitosamente
+pause
---
+++ crear-carpetas.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir -p src/main/java/com/example/flightreservations
+mkdir -p src/main/resources
+echo "Carpetas creadas exitosamente"
